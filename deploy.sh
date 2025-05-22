19cac2f547b065fae004835540af4c1e66c6689a
Siracgul <sirac.gul@hotmail.com>
2025-05-22 22:35:40 +0100

    🟢 Test deploy - index.html updated

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
-GREEN_PORT=8081
-BLUE_PORT=8082
-
-CURRENT_COLOR=$(cat /home/ubuntu/current_color.txt)
-
-if [ "$CURRENT_COLOR" == "blue" ]; then
-  TARGET_COLOR="green"
-  TARGET_PORT=$GREEN_PORT
+# Aktif ortamı belirle
+if [ -f /home/ubuntu/current_color.txt ]; then
+  CURRENT_COLOR=$(cat /home/ubuntu/current_color.txt)
 else
-  TARGET_COLOR="blue"
-  TARGET_PORT=$BLUE_PORT
+  CURRENT_COLOR=green
 fi
 
-# Container'ı temizle
+if [ "$CURRENT_COLOR" = "blue" ]; then
+  TARGET_COLOR=green
+  TARGET_PORT=8081
+else
+  TARGET_COLOR=blue
+  TARGET_PORT=8080
+fi
+
+# Container’ı temizle
 docker stop $TARGET_COLOR || true
 docker rm $TARGET_COLOR || true
 
-# Yeni container'ı başlat
+# Yeni container’ı başlat
 docker run -d \
   --name $TARGET_COLOR \
   -p 80:80 \
   -v /home/ubuntu/blue-green-deploy:/usr/share/nginx/html \
   nginx
 
-# Yeni ortamı aktif olarak kaydet
+# Yeni ortamı kaydet
 echo "$TARGET_COLOR" > /home/ubuntu/current_color.txt
-
 echo "✅ Yeni ortam deploy edildi: $TARGET_COLOR"
